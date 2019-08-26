fac435bf0c1efa66a9ddc73b24ce1fa8818f0b45
ckr <chaikairui@163.com>
2019-08-26 18:10:19 +0800

    提交项目框架

---
+++ src/main/java/com/ckr/ipfsfile/IpfsfileApplication.java
@@ -0,0 +1,13 @@
+package com.ckr.ipfsfile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IpfsfileApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IpfsfileApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ckr/ipfsfile/IpfsfileApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ckr.ipfsfile;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IpfsfileApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
