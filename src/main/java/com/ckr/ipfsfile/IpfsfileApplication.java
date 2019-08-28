ef9e0d7786917913637bf42fea19132aab40b7e9
ckr <chaikairui@163.com>
2019-08-28 17:51:13 +0800

    ipfs 工具类

--- src/main/java/com/ckr/ipfsfile/IpfsfileApplication.java
+++ src/main/java/com/ckr/ipfsfile/IpfsfileApplication.java
@@ -2,8 +2,9 @@ package com.ckr.ipfsfile;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class IpfsfileApplication {
 
     public static void main(String[] args) {
--- src/test/java/com/ckr/ipfsfile/IpfsfileApplicationTests.java
+++ src/test/java/com/ckr/ipfsfile/IpfsfileApplicationTests.java
@@ -5,12 +5,16 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class IpfsfileApplicationTests {
 
+
     @Test
     public void contextLoads() {
+
     }
+
 
 }
